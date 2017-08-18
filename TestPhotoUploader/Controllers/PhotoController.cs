a2197ba4cc01838c749c092abcc9f5c36a8e42bc
Roman <roman.koliada@ssa-outsourcing.com>
2017-08-18 12:12:20 +0300

    Implement analyzing photos

--- TestPhotoUploader/Controllers/PhotoController.cs
+++ TestPhotoUploader/Controllers/PhotoController.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.Azure;
+using Microsoft.ProjectOxford.Vision;
+using Microsoft.ProjectOxford.Vision.Contract;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,11 +16,16 @@ namespace TestPhotoUploader.Controllers
     public class PhotoController : Controller
     {
         private CloudBlobClient _blobClient = null;
+        private VisionServiceClient _visionClient = null;
+
         public PhotoController()
         {
-            string connectionString = CloudConfigurationManager.GetSetting("StorageConnectionString");
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            string storageConnectionString = CloudConfigurationManager.GetSetting("StorageConnectionString");
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnectionString);
             _blobClient = storageAccount.CreateCloudBlobClient();
+
+            string visionAPIKey = CloudConfigurationManager.GetSetting("VisionAPIKey");
+            _visionClient = new VisionServiceClient(visionAPIKey, "https://westeurope.api.cognitive.microsoft.com/vision/v1.0");
         }
 
         // GET: Photo
@@ -25,24 +35,28 @@ namespace TestPhotoUploader.Controllers
         }
 
         [HttpPost]
-        public ActionResult Upload(HttpPostedFileBase photo)
+        public async Task<ActionResult> Upload(HttpPostedFileBase photo)
         {
             string message = "File is empty.";
             if (photo != null && photo.ContentLength > 0)
             {
                 try
                 {
-                    CloudBlobContainer container = _blobClient.GetContainerReference("test-photo-analyzing");
-                    container.CreateIfNotExists();
+                    using (MemoryStream copyStream = new MemoryStream())
+                    {
+                        photo.InputStream.CopyTo(copyStream);
 
-                    string folderName = GetTodayFolder();
-                    CloudBlobDirectory todayFolder = container.GetDirectoryReference(folderName);
+                        var blobTask = UploadPhotoAsync(photo);
 
-                    string name = CreateBlobName(photo);
-                    CloudBlockBlob blockBlob = todayFolder.GetBlockBlobReference(name);
+                        var analysisTask = AnalyzeImageAsync(copyStream);
 
-                    blockBlob.UploadFromStream(photo.InputStream);
-                    message = $"Photo {name} was uploaded";
+                        await Task.WhenAll(blobTask, analysisTask);
+
+                        var result = analysisTask.Result;
+                        //TODO: Implement ToString and saving results. Add a separate model
+                        message = $"Photo {blobTask.Result.Name} was uploaded. " +
+                            $"Description: {string.Join(",", result.Description.Captions.Select(x => x.Text))}";
+                    }
                 }
                 catch (Exception ex)
                 {
@@ -51,6 +65,41 @@ namespace TestPhotoUploader.Controllers
             }
 
             return View(nameof(Index), model: message);
+        }
+
+        //TODO: Move it to separate service
+        private async Task<AnalysisResult> AnalyzeImageAsync(Stream photoStream)
+        {
+            photoStream.Seek(0, SeekOrigin.Begin);
+
+            var parameters = new List<VisualFeature> {
+                    //VisualFeature.Adult,
+                    //VisualFeature.Categories,
+                    VisualFeature.Color,
+                    VisualFeature.Description,
+                    //VisualFeature.Faces,
+                    //VisualFeature.ImageType,
+                    VisualFeature.Tags
+                };
+
+            return await _visionClient.AnalyzeImageAsync(photoStream, parameters, null);
+        }
+
+        //TODO: Move it to separate service
+        private async Task<CloudBlockBlob> UploadPhotoAsync(HttpPostedFileBase photo)
+        {
+            CloudBlobContainer container = _blobClient.GetContainerReference("test-photo-analyzing");
+            container.CreateIfNotExists();
+
+            string folderName = GetTodayFolder();
+            CloudBlobDirectory todayFolder = container.GetDirectoryReference(folderName);
+
+            string name = CreateBlobName(photo);
+            CloudBlockBlob blockBlob = todayFolder.GetBlockBlobReference(name);
+
+            await blockBlob.UploadFromStreamAsync(photo.InputStream);
+
+            return blockBlob;
         }
 
         private string GetTodayFolder() => DateTime.Today.ToString("dd-MM-yyyy");
